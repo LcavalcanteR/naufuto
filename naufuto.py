1a04acb94a897740075ec02a2cee966d9d55090a
Lucas Calvalcante <cavalcantelucas@outlook.com>
2019-06-05 23:53:07 -0300

    Create naufuto.py
    Yes, it is an emoji in the code.

---
+++ naufuto.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#um abraço pro tio Naufel
+import time
+import telepot
+#Reference: http://unicode.org/emoji/charts/full-emoji-list$
+falante = u'🔊'
+naufalante = falante+falante+falante
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print 'recebi isso: %s' % command 
+    print 'do chat: %s' % chat_id
+    
+    if command == naufalante:
+        bot.sendSticker(chat_id, 'CAADAQADJwEAAiSbWAa1_wzLlUwZdwI'
+    elif command == 'aaaa':
+        bot.sendMessage(chat_id, 'aaaaaaaaaaaaaaaaaaaaaaaa'
+
+bot = telepot.Bot('TOKEN')
+
+MessageLoop(bot, handle).run_as_thread()
+print 'aaaaaaaaaaaaaAAAAAAAAAAA'
+
+while 1:
+    time.sleep(10)
